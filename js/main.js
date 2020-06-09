358670444bc257ec0dfe4aa03f8fdfc946ec1b54
Антон А. Клепиковский <ed-KlepikovskiyAA@kazan.local>
2020-06-09 15:13:46 +0300

    feat: add section project and swiper-slider in her

--- js/main.js
+++ js/main.js
@@ -84,8 +84,8 @@ $(document).ready(function () {
   var prev = $('.swiper-button-prev');
   var bullets =$('.swiper-pagination')
 
-  next.css('left', prev.width() + 10 + bullets.width() + 10 )
-  bullets.css('left', prev.width() + 10 )
+  next.css('left', prev.width() + 25 + bullets.width() + 10 )
+  bullets.css('left', prev.width() + 25 )
 
   var button = $('#button-up');	
   $(window).scroll (function () {
